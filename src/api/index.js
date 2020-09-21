bb5ab2b1e86d5a73facdfa71c946ff9a9da99955
yacchiDAO <okamura@net.lab.uec.ac.jp>
2020-09-22 00:26:45 +0900

    状態モデリングとデータフロー雛形

---
+++ src/api/index.js
@@ -0,0 +1,9 @@
+import Auth from './auth'
+import List from './List'
+import Task from './Task'
+
+export {
+  Auth,
+  List,
+  Task
+}
---
+++ src/store/mutations.js
@@ -0,0 +1,33 @@
+import * as types from './mutation-types'
+
+export default {
+  [types.AUTH_LOGIN](state, payload) {
+    // TODO:
+    throw new Error('AUTH_LOGIN')
+  },
+
+  [types.FETCH_ALL_TASKLIST](state, payload) {
+    // TODO:
+    throw new Error('FETCH_ALL_TASKLIST')
+  },
+
+  [types.ADD_TASK](state, payload) {
+    // TODO:
+    throw new Error('ADD_TASK')
+  },
+
+  [types.UPDATE_TASK](state, payload) {
+    // TODO:
+    throw new Error('UPDATE_TASK')
+  },
+
+  [types.REMOVE_TASK](state, payload) {
+    // TODO:
+    throw new Error('REMOVE_TASK')
+  },
+
+  [types.AUTH_LOGOUT](state, payload) {
+    // TODO:
+    throw new Error('AUTH_LOGOUT')
+  }
+}
