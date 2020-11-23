58ccd61498343d0d460bc991a53c627767c514d9
yacchiDAO <okamura@net.lab.uec.ac.jp>
2020-11-23 17:36:25 +0900

    10.7 エラーハンドリング

--- src/components/templates/KbnBoardView.vue
+++ src/components/templates/KbnBoardView.vue
@@ -1,3 +1,15 @@
 <template>
   <p>ボードページ</p>
 </template>
+
+<script>
+/* eslint-disable */
+export default {
+  name: 'KbnBoardView',
+
+  render (h) {
+    throw new Error('レンダリングに失敗しました！')
+  }
+}
+/* eslint-enable */
+</script>
--- src/main.js
+++ src/main.js
@@ -3,10 +3,20 @@
 import Vue from 'vue'
 import 'es6-promise/auto' // プロミスをポリフィルする
 import App from './App'
+import ErrorBoundary from './ErrorBoundary.vue'
 import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false
+
+// ErrorBoundaryのインストール
+Vue.component(ErrorBoundary.name, ErrorBoundary)
+
+Vue.config.errorHandler = (err, vm, info) => {
+  console.error('errorHandler err:', err)
+  console.error('errorHandler vm:', vm)
+  console.error('errorHandler info:', info)
+}
 
 /* eslint-disable no-new */
 new Vue({
